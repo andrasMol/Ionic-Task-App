5a9470687d1a91b6ca6fbb940a1ac9149c574f66
Andrew Moldovan <anmoldov@microsoft.com>
2016-01-16 21:19:43 -0800

    added in a service cache to hold the tasks that we have and created the individual task page which fetches from cache or from server if not in cache

--- www/js/myTask.controller.js
+++ www/js/myTask.controller.js
@@ -1,4 +1,16 @@
 angular.module('tasks.controllers')
 
-.controller('MyTaskCtrl', function($scope, $stateParams) {
+.controller('MyTaskCtrl', function($scope, $stateParams, MyTasksService) {
+
+  if ($stateParams.taskId) {
+    MyTasksService.getMyTask($stateParams.taskId).then(
+      function success(task) {
+        $scope.myTask = task;
+      },
+      function failure(err) {
+        console.log(err);
+        $scope.error = 'Sorry, something went wrong';
+      }
+    )
+  }
 });
--- www/js/myTasks.service.js
+++ www/js/myTasks.service.js
@@ -1,11 +1,44 @@
 angular.module('tasks.services', [])
 
-.service('MyTasksService', function($http) {
+.service('MyTasksService', function($http, $q) {
+  var allTasks = {};
+
+  this.getMyTask = function(id) {
+    // this will return a promise since we might not have the item in cache yet
+    var deferred = $q.defer();
+    if (allTasks[id]) {
+      deferred.resolve(allTasks[id]);
+    } else {
+      this.getMyTasks().then(
+        function success(response) {
+          if (allTasks[id]) {
+            deferred.resolve(allTasks[id]);
+          } else {
+            deferred.reject();
+          }
+        },
+        function failure(err) {
+          deferred.reject(err);
+        }
+      );
+    }
+    return deferred.promise;
+  }
 
   this.getMyTasks = function() {
     return $http({
       method: 'GET',
       url: 'https://teamgantt.com/gantt/_andrew_/json.php'
-    });
+    }).then(
+      function success(response) {
+        if (response.data.tasks) {
+          for (var i = 0; i < response.data.tasks.length; i++) {
+            allTasks[response.data.tasks[i].id] = response.data.tasks[i];
+          }
+        }
+        // make sure we still return this line as this returns it as part of the promise chain
+        return response;
+      }
+    );
   }
 });
